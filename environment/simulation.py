6c3d7e5ee2185851e4242d1105bf25c3dede0a33
sayhanne <sayhannee@gmail.com>
2023-12-30 15:27:25 +0300

    created functions, offsets are updated

--- environment/simulation.py
+++ environment/simulation.py
@@ -19,13 +19,14 @@ class Environment:
         self.trajectory_data = {'pos': [], 'vel': [], 'torque': []}
 
         # Objects
-        self.table = self.world.addBox(x=1, y=1, z=0.3, mass=10, material='default', collision_group=1)
+        self.table: raisim.Box = self.world.addBox(x=1, y=1, z=0.3, mass=10, material='default', collision_group=1)
         table_pos = [0.7, 0, 0.15]
         self.table.setPosition(table_pos)
         self.table.setName("table")
 
-        self.cube = self.world.addBox(x=0.05, y=0.05, z=0.05, mass=0.5, material='default', collision_group=2)
-        cube_pos = [0.5, 0.2, 0.325]
+        self.cube: raisim.Box = self.world.addBox(x=0.05, y=0.05, z=0.05, mass=0.5, material='default',
+                                                  collision_group=2)
+        cube_pos = [0.5, -0.2, 0.325]
         self.cube.setPosition(cube_pos)
         self.cube.setName("cube")
         self.cube.setAppearance("red")
@@ -35,6 +36,12 @@ class Environment:
         self.d_gain_ref = np.array([10, 50, 10, 50, 10, 10, 10, 0.01, 0.01])
         self.p_gain = np.array([200, 1000, 200, 2000, 200, 200, 200, 0.1, 0.1])
         self.d_gain = np.array([10, 50, 10, 50, 10, 10, 10, 0.01, 0.01])
+
+        # q_ref, prev_q_ref, dq_ref, prev_dq_ref
+        self.prev_dq_ref = np.zeros((7, 1))
+        self.dq_ref = np.zeros((7, 1))
+        self.prev_q_ref = np.zeros((7, 1))
+        self.q_ref = np.expand_dims(np.array([0, -0.785, 0, -2.356, 0, 1.5708, 0.7853]), axis=1)
 
     def record_data(self):
         # time = self.world.getSimulationTime()self.
@@ -49,14 +56,28 @@ class Environment:
 
     def reset_robot(self):
         # initial position
-        angle = np.array([0, -0.785, 0, -2.356, 0, 1.5708, 0.7853, 0, 0, 0.1, 0.1])  # Last 2 joints are prismatic.
+        angle = np.array([0, -0.785, 0, -2.356, 0, 1.5708, 0.7853, 0, 0, 0.1,
+                          0.1])  # Last 2 joints are prismatic. Hand joint and joint 8 are fixed.
         self.robot.setGeneralizedCoordinate(angle[:-2])
         self.robot.setPdGains(self.p_gain_ref, self.d_gain_ref)
         self.robot.setPdTarget(angle[:-2], np.zeros([9]))
-        return self.robot.getFramePosition(11)  # return initial end-effector pos
+        return self.robot.getFramePosition(11)  # return hand position
 
-    def spawn_object(self):
-        pass
+    def get_pick_pos(self):
+        cube_pos = self.cube.getPosition()
+        pos_end = cube_pos
+        # pos_end[0] += 0.01     # x-axis offset
+        pos_end[1] += np.sign(pos_end[1]) * 0.02  # y-axis offset
+        pos_end[2] += 0.14  # z-axis offset
+        return pos_end
+
+    def control(self, jac, twist):
+        self.prev_dq_ref = self.dq_ref
+        self.dq_ref = np.linalg.lstsq(jac, twist, rcond=None)[0]
+        self.prev_q_ref = self.q_ref
+        self.q_ref = np.add(np.add(self.prev_dq_ref, self.dq_ref) * dt * 0.5, self.prev_q_ref)  # Integral
+        self.set_force(target_joint_angle=self.q_ref, target_joint_vel=self.dq_ref,
+                       p_gain=self.p_gain, d_gain=self.d_gain)
 
     def trajectory_planning(self, real_time, t_start, t_end, timestep, pos_start, pos_end,
                             euler_start=None, euler_end=None):
@@ -94,40 +115,39 @@ class Environment:
         self.robot.setGeneralizedForce(tau * 10)
         self.record_data()
 
+    def open_gripper(self):
+        angles = self.robot.getGeneralizedCoordinate()
+        angles[-2:] = [0.035, 0.035]
+        self.robot.setGeneralizedCoordinate(angles)
+
+    def close_gripper(self):
+        angles = self.robot.getGeneralizedCoordinate()
+        angles[-2:] = [0., 0.]
+        self.robot.setGeneralizedCoordinate(angles)
+
 
 if __name__ == '__main__':
     dt = 0.001
     env = Environment(timestep=dt)
-    print(env.robot.getFrameIdxByName("panda_hand_joint"))
-
-    prev_dq_ref = np.zeros((7, 1))
-    dq_ref = np.zeros((7, 1))
-    prev_q_ref = np.zeros((7, 1))
-    q_ref = np.expand_dims(np.array([0, -0.785, 0, -2.356, 0, 1.5708, 0.7853]), axis=1)
 
     server = raisim.RaisimServer(env.world)
     server.launchServer(8080)
     pos_start_ = env.reset_robot()
-    cube_pos = env.cube.getPosition()
-    pos_end_ = cube_pos
-    # pos_end_[0] += 0.01     # x-axis offset
-    # pos_end_[1] += 0.02     # y-axis offset
-    pos_end_[2] += 0.14  # z-axis offset
+    pos_end_ = env.get_pick_pos()
     euler_start_ = [0., 0., 0.]
-    euler_end_ = [2, 2, 0.2]
+    # euler_end_ = [2, 2, 0.2]
+    ts = 1
+    reach_time = 5
+    te = ts + reach_time
 
     while True:
         rt = env.world.getWorldTime()
         server.integrateWorldThreadSafe()
-        jac_res, twist_res = env.trajectory_planning(real_time=rt, t_start=3, t_end=10, timestep=dt,
+        jac_res, twist_res = env.trajectory_planning(real_time=rt, t_start=ts, t_end=te, timestep=dt,
                                                      pos_start=pos_start_, pos_end=pos_end_,
                                                      euler_start=euler_start_, euler_end=euler_start_)
-        prev_dq_ref = dq_ref
-        dq_ref = np.linalg.lstsq(jac_res, twist_res, rcond=None)[0]
-        prev_q_ref = q_ref
-        q_ref = np.add(np.add(prev_dq_ref, dq_ref) * dt * 0.5, prev_q_ref)  # Integral
-        env.set_force(target_joint_angle=q_ref, target_joint_vel=dq_ref,
-                      p_gain=env.p_gain, d_gain=env.d_gain)
+        env.control(jac_res, twist_res)
+
         time.sleep(dt)
     # np.save('traj.npy', env.trajectory_data)
     # server.killServer()
